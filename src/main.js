6b405bff23dd54690ea357023dd13ff37f8e37f4
Alexis CHOLLET-EXT <alexis.chollet-ext@fnacdarty.com>
2018-06-01 16:05:14 +0200

    Adding vue-google- map package

--- src/main.js
+++ src/main.js
@@ -1,6 +1,14 @@
 import Vue from 'vue'
+import * as VueGoogleMaps from 'vue2-google-maps'
 import App from './App'
 import router from './router'
+
+Vue.use(VueGoogleMaps, {
+  load: {
+    key: 'AIzaSyAVQqgfPAvDMoa9SSUyQzQqA2nMNgeZlkA',
+    libraries: 'places'
+  }
+})
 
 Vue.config.productionTip = false
 
